d913b48a1d8420c324a48847cb3ede07d41dd369
helton.godoy <36382889+heltongodoy@users.noreply.github.com>
2023-08-29 07:10:27 -0400

    Add files via upload

---
+++ install_gnome_minimal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt install gnome-session gnome-shell gnome-keyring libpam-gnome-keyring gdm3 gnome-terminal nautilus gnome-online-accounts gnome-tweaks gedit htop fonts-cantarell fonts-fantasque-sans network-manager-gnome network-manager-openvpn-gnome gnome-software gnome-software-common gnome-software-plugin-flatpak;
